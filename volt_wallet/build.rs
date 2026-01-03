dafc5ba3f45ea39fac1e94d29f7788654deb8ace
eslamsheref5000 <eslamsheref5000@gmail.com>
2026-01-04 01:32:47 +0200

    Fix: Only link resources.o on Windows

--- volt_wallet/build.rs
+++ volt_wallet/build.rs
@@ -2,7 +2,10 @@ use std::env;
 use std::path::Path;
 
 fn main() {
-    let dir = env::var("CARGO_MANIFEST_DIR").unwrap();
-    let res_path = Path::new(&dir).join("resources.o");
-    println!("cargo:rustc-link-arg={}", res_path.display());
+    #[cfg(target_os = "windows")]
+    {
+        let dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+        let res_path = Path::new(&dir).join("resources.o");
+        println!("cargo:rustc-link-arg={}", res_path.display());
+    }
 }
